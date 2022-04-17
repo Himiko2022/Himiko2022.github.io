f98d8e56029d76b40832ff2aa206c33919f17466
Himiko2022 <100595869+Himiko2022@users.noreply.github.com>
2022-04-17 20:51:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+
+function sayHello(){
+  var response = prompt("请输入您的名字：");
+  alert("您好"+ response + " ，欢迎咨询臭狗就业指南！");
+}
+
+sayHello();
+
+function answer1(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("哇哦！学习当出租车司机的"+ response + "一定很有方向感！(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer2(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("难以启齿......"+ response + "要学习当牧羊犬吗，可是它聪明的样子看起来跟本不用教呢！(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer3(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("有点难度。"+ response + "当散财童子也许会更合适。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer4(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("干！"+ response + "当救生员真是酷毙了！(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer5(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("真的要让"+ response + "学拳击吗...要不要再考虑看看。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer6(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("这样看来，"+ response + "似乎是柔韧性的高手。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer7(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("嗯嗯。让被修理转变为修理对于"+ response + "来说也是一件好事。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer8(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("诶呀！"+ response + "可真是会选啦～光宗耀祖的职业！(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer9(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("了不起，"+ response + "的志向让人肃然起敬。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer10(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("嗯..."+ response + "的选择似乎暗含深意。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer11(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("相信强壮的"+ response + "一定能胜任。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer12(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert( response + "调出来的酒相信没有人类能够抵抗！(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer13(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("看不出来呢，"+ response + "原来也是很有耐心的孩子。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer14(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("喜欢探索的"+ response + "，这样的工作一定很适合它。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer15(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("很有钱途的职业。拭目以待，等待"+ response + "学成归来......(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer16(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("说实话，在派送食物的专业里我们鲜少有成功毕业的学生。期待"+ response + "的表现！(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer17(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("对于年纪轻轻的"+ response + "，一定的经验能够帮助它看清资本世界。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
+
+function answer18(){
+  var response = prompt("请输入您家小孩的名字来报名：");
+  alert("不得不说，当上总监的"+ response + "比以往更迷人了。(感谢您的报名，4月暂不开学，具体开学时间请留意网站通知。)");
+}
